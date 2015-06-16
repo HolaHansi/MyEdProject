ad6c858cd122e9c9c70ed20e2f735b81db9fc3a0
HolaHansi <s1310721@sms.ed.ac.uk>
2015-06-16 09:23:23 +0100

    it's tuesday, tuesday, gotta get down on

--- InternProject/settings.py
+++ InternProject/settings.py
@@ -56,6 +56,19 @@ from .celery import CELERYBEAT_SCHEDULE
 CELERY_TIMEZONE = 'Europe/London'
 
 
+# REST_FRAMEWORK SETTINGS
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+
+
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
