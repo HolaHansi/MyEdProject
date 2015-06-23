87be46c10fdcfdac8ec49049d25d90a7cf14423d
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-06-23 11:26:48 +0100

    Added sorting by both distance and availability
    I actually used Inf2B!  Hoorah for normalisation and heuristics!

--- pc/models.py
+++ pc/models.py
@@ -6,7 +6,9 @@ class PC_Space(models.Model):
     free = models.IntegerField(default=0)
     seats = models.IntegerField(default=0)
     group = models.CharField(max_length=200)
-    ratio = models.FloatField(default=0)
+    ratio = models.FloatField(default=0) # the ratio of computers available to computers in use
+                                         # ie computersAvailable/computersInUse
+                                         # so 1 is an empty room, 0 is a full room
     longitude = models.FloatField(default=0)
     latitude = models.FloatField(default=0)
     id = models.AutoField(primary_key=True)
@@ -17,19 +19,44 @@ class PC_Space(models.Model):
     def __str__(self):
         return self.location
 
+    # ratio getter
+    def get_ratio(self):
+        return self.ratio
+
+    # calculate the distance between the current building and the inputted point
+    # parameters: long1 - the longitude of the user
+    #             lat1 - the latitude of the user
     def get_distance(self, long1, lat1):
         R = 6371000 # metres
+        # convert all coordinates to radians
         t1 = self.toRadians(lat1)
         t2 = self.toRadians(self.latitude)
         dt = self.toRadians(self.latitude - lat1)
         dl = self.toRadians(self.longitude - long1)
-
+        # do some clever maths which the internet told me was correct
         a = math.sin(dt / 2) * math.sin(dt / 2) + math.cos(t1) * math.cos(t2) * math.sin(dl / 2) * math.sin(dl / 2)
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        # return the distance between the points
         return R * c
-
+    # converts from degrees to radians
+    # parameters: x - the value in degrees to be converted
     def toRadians(self, x):
         return x*math.pi/180
+
+    # heuristic function for when both distance and empty are selected
+    # calculates a heuristic value based on the normalised distance and emptiness
+    # parameters: averageDistance - the average distance to all the buildings
+    #             averageRatio - the average emptiness ratio of all the buildings
+    #             standardDeviationDistance - the standard deviation of the distance to all the buildings
+    #             standardDeviationRatio - the standard deviation of the emptiness ratio of all the buildings
+    #             long - the user's current longitude
+    #             lat - the user's current latitude
+    def get_heuristic(self,averageDistance,averageRatio,standardDeviationDistance,standardDeviationRatio, long, lat):
+        # normalise the building's distance and ratio (normalised mean=0, normalised SD=1)
+        normalisedDistance=(self.get_distance(long,lat)-averageDistance)/standardDeviationDistance
+        normalisedRatio=(self.get_ratio()-averageRatio)/standardDeviationRatio
+        # we want a small difference and a large ratio
+        return normalisedDistance-normalisedRatio
 
 
 
--- pc/views.py
+++ pc/views.py
@@ -17,28 +17,55 @@ class JSONResponse(HttpResponse):
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
-
-
 def test(request):
     return render(request, 'pc/jsonTest.html')
-
 
 def filter_suggestions(request):
     if request.method == "GET":
 
+        # filter by group:
         if request.GET['group'] != 'nopref':
             data = PC_Space.objects.filter(group=request.GET['group'])
         else:
             data = PC_Space.objects.all()
 
+        # don't suggest any full rooms:
         data = data.exclude(ratio=0)
 
+        # if sorting by location
         if (request.GET['nearby']=='true'):
+            # get the user's latitude and longitude
             usr_longitude = float(request.GET['longitude'])
             usr_latitude = float(request.GET['latitude'])
+            # sort the buildings based on distance from user, closest first
             data = sorted(data, key=lambda x: x.get_distance(long1=usr_longitude, lat1=usr_latitude))
 
-        if (request.GET['empty']=='true'):
+            # if sorting by both location and emptiness
+            if (request.GET['empty']=='true'):
+                # calculate the average distance and ratio:
+                averageDistance=0
+                averageRatio=0
+                i=0
+                for x in data:
+                    averageDistance=averageDistance + x.get_distance(long1=usr_longitude, lat1=usr_latitude)
+                    averageRatio=averageRatio + x.get_ratio()
+                    i+=1
+                averageDistance=averageDistance/i
+                averageRatio=averageRatio/i
+                # calculate the standard deviation of distance and ratio
+                sdDistance=0
+                sdRatio=0
+                for x in data:
+                    sdDistance=sdDistance + (x.get_distance(long1=usr_longitude, lat1=usr_latitude)-averageDistance)**2
+                    sdRatio=sdRatio + (x.get_ratio()-averageRatio)**2
+                sdDistance=(sdDistance/i)**0.5
+                sdRatio=(sdRatio/i)**0.5
+                # sort the data based on both distance and ratio using a heuristic function of the normalised distance and ratio
+                data = sorted(data,key=lambda x:x.get_heuristic(averageDistance,averageRatio,sdDistance,sdRatio,usr_longitude,usr_latitude))
+
+        # if sorting only by emptiness
+        elif (request.GET['empty']=='true'):
+            # sort by ratio, emptiest first
             data = sorted(data, key=lambda x:x.ratio, reverse=True)
 
         serializer = PC_Space_Serializer(data, many=True)
