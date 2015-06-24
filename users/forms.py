b5c8fa96c13a072b4cd57f6f0c741af0640fc28e
HolaHansi <s1310721@sms.ed.ac.uk>
2015-06-24 15:00:28 +0100

    registration added

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,24 @@
-from django.forms import ModelForm
-from .models import Entry
+from django import forms
+from .models import Entry, UserProfile
+from django.contrib.auth.models import User
 
-class EntryForm(ModelForm):
+
+class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['title', 'text']
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['is_a_john']
+
+
+
--- users/models.py
+++ users/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from rooms.models import Bookable_Room
+from pc.models import PC_Space
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
+    room_favourites = models.ManyToManyField(Bookable_Room, related_name='room_fav')
+    pc_favourites = models.ManyToManyField(PC_Space, related_name='pc_fav')
+    room_history = models.ManyToManyField(Bookable_Room, related_name='room_his')
+    pc_history = models.ManyToManyField(PC_Space, related_name='pc_hist')
 
-    is_a_john = models.BooleanField(default=True)
+    # for testing
+    is_a_john = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.user.username
