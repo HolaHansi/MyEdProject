8986ebf5306265ffa03e53088084d6157dc9fba1
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-06-29 14:44:27 +0100

    Added more keys manually to improve the merge
    Probably most importantly, it now includes the library

--- rooms/table.py
+++ rooms/table.py
@@ -123,15 +123,116 @@ def update_building_table():
     url = "http://webproxy.is.ed.ac.uk/web-proxy/maps/portal.php" #smaller lat/long
     buildings = requests.get(url)
 
+    # extra keys which aren't in the lat/long database, despite the fact their buildings are
+    # yeah, the database is kind of rubbish
+    # I had to look these ones up manually, so it's not a complete list,
+    # nor obviously will it update, but it's a wee help at least
+    extraKeys={
+        'Main University Library':'0224',
+        'Business School':'0226',
+        'Grant Institute':'0633',
+        'Sanderson Building':'0601',
+        'Michael Swann Building':'0612',
+        'Dugald Stewart Building':'0283',
+        "Chancellor's Building":'2701',
+        'Chrystal MacMillan Building':'0112',
+        'Buccleuch Place':'0261', # 31 Buccleuch Place
+        # 'Buccleuch Place':'0244', # 14 Buccleuch Place
+        # 'Buccleuch Place':'0252', # 22 Buccleuch Place
+        # 'Buccleuch Place':'0254', # 24 Buccleuch Place
+        # 'Buccleuch Place':'0245', # 15 Buccleuch Place
+        # 'Buccleuch Place':'0247', # 17 Buccleuch Place
+        # 'Buccleuch Place':'0260', # 30 Buccleuch Place
+        'William Rankine Building':'0668',
+        'C.H. Waddington Building':'0670',
+        'Psychology Building':'0209',
+        'Hospital for Small Animals':'0719',
+        "Old Surgeons' Hall":'0313',
+        'Old Infirmary (Geography)':'0311',
+        'Evolution House':'0424',
+        'Hunter Building':'0423',
+        'ECA Main Building':'0421',
+        'ECA Architecture Building':'0422',
+        'Economics, School of':'0260',
+        'Noreen and Kenneth Murray Library':'0636',
+        'Darwin Learning & Teaching Cluster':'0611',
+        'George Square (1)':'0208', # 1 George Square
+        'George Square (2-15)':'0209', # 7 George Square
+        'George Square (16-27)':'0214', # 16-20 George Square
+        # 'George Square (16-27)':'0219', # 21 George Square
+        'Medical Education Centre':'2305',
+        "St Leonard's Land":'0564'
+    }
 
     for building in buildings.json()["locations"]:
-        # we're not interested in buildings without an abbreviation, as this is a critical component for linking
-        # the this tables with Room_Feed.
-        if 'abbreviation' in building.keys():
-            abbreviation = building["abbreviation"][:4]
+        # we're not interested in buildings without an abbreviation
+        # as this is the only way we can link the tables with Room_Feed.
+        if 'abbreviation' in building.keys() or ('name' in building.keys() and building['name'] in extraKeys):
             longitude = float(building["longitude"])
             latitude = float(building["latitude"])
             building_name = building["name"]
+            if 'abbreviation' in building.keys():
+                abbreviation = building["abbreviation"][:4]
+            else:
+                abbreviation = extraKeys[building['name']]
+
+                # Rename any buildings called strange things in the lat/long database
+                if (building['name'])=='George Square (2-15)':
+                    building_name='7 George Square'
+                elif (building['name'])=='George Square (1)':
+                    building_name='1 George Square'
+                elif (building['name'])=='George Square (1)':
+                    building_name='1 George Square'
+                # Two George Square buildings both use the same 'building' name, the lat/long database is kinda rubbish.
+                elif (building['name'])=='George Square (16-27)':
+                    #create second object and save to database
+                    obj = Building_Feed(abbreviation='0219',
+                                 longitude=longitude,
+                                 latitude=latitude,
+                                 building_name='21 George Square')
+                    obj.save()
+                    #continue with first object
+                    building_name= '16-20 George Square'
+                # Think that's bad?  There's loads of Buccleuch Place ones all using the same lat/long...
+                elif (building['name'])=='Buccleuch Place':
+                    #create second object and save to database
+                    obj = Building_Feed(abbreviation='0244',
+                                 longitude=longitude,
+                                 latitude=latitude,
+                                 building_name='14 Buccleuch Place')
+                    obj.save()
+                    #create third object and save to database
+                    obj = Building_Feed(abbreviation='0252',
+                                 longitude=longitude,
+                                 latitude=latitude,
+                                 building_name='22 Buccleuch Place')
+                    obj.save()
+                    #create fourth object and save to database
+                    obj = Building_Feed(abbreviation='0254',
+                                 longitude=longitude,
+                                 latitude=latitude,
+                                 building_name='24 Buccleuch Place')
+                    obj.save()
+                    #create fifth object and save to database
+                    obj = Building_Feed(abbreviation='0245',
+                                 longitude=longitude,
+                                 latitude=latitude,
+                                 building_name='15 Buccleuch Place')
+                    obj.save()
+                    #create sixth object and save to database
+                    obj = Building_Feed(abbreviation='0247',
+                                 longitude=longitude,
+                                 latitude=latitude,
+                                 building_name='17 Buccleuch Place')
+                    obj.save()
+                    #create seventh object and save to database
+                    obj = Building_Feed(abbreviation='0260',
+                                 longitude=longitude,
+                                 latitude=latitude,
+                                 building_name='30 Buccleuch Place')
+                    obj.save()
+                    # continue with first object
+                    building_name='31 Buccleuch Place'
 
             #create object and save to database
             obj = Building_Feed(abbreviation=abbreviation,
