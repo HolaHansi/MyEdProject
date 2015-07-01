b96760bba2148d537b6bfe79fedfcd35a8aebae3
HolaHansi <s1310721@sms.ed.ac.uk>
2015-07-01 17:05:28 +0100

    changes to requirements.txt

--- InternProject/settings.py
+++ InternProject/settings.py
@@ -111,25 +111,12 @@ WSGI_APPLICATION = 'InternProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Mysql',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 # THIS IS HEROKU SETTINGS:
 # # Parse database configuration from $DATABASE_URL
 # import dj_database_url
