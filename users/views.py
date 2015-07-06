f90e1d5ec6d5b14c0da62923c4f433ad48d44173
HolaHansi <s1310721@sms.ed.ac.uk>
2015-07-06 17:06:45 +0100

    environment variables in use!

--- users/views.py
+++ users/views.py
@@ -26,8 +26,6 @@ class JSONResponse(HttpResponse):
 
 
 
-
-
 def index(request):
     return render(request, 'users/index.html')
 
@@ -167,55 +165,64 @@ def logout(request):
 
 def register(request):
 
-    # A boolean value for telling the template whether the registration was successful.
-    # Set to False initially. Code changes value to True when registration succeeds.
-    registered = False
+    if settings.ENV == 'development':
 
-    # If it's a HTTP POST, we're interested in processing form data.
-    if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
-        user_form = UserForm(data=request.POST)
+        # A boolean value for telling the template whether the registration was successful.
+        # Set to False initially. Code changes value to True when registration succeeds.
+        registered = False
 
-        # If the two forms are valid...
-        if user_form.is_valid():
-            # Save the user's form data to the database.
-            user = user_form.save()
+        # If it's a HTTP POST, we're interested in processing form data.
+        if request.method == 'POST':
+            # Attempt to grab information from the raw form information.
+            # Note that we make use of both UserForm and UserProfileForm.
+            user_form = UserForm(data=request.POST)
 
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
-            user.set_password(user.password)
-            user.save()
+            # If the two forms are valid...
+            if user_form.is_valid():
+                # Save the user's form data to the database.
+                user = user_form.save()
+
+                # Now we hash the password with the set_password method.
+                # Once hashed, we can update the user object.
+                user.set_password(user.password)
+                user.save()
 
 
-            # # Now sort out the UserProfile instance.
-            # # Since we need to set the user attribute ourselves, we set commit=False.
-            # # This delays saving the model until we're ready to avoid integrity problems.
-            # profile = profile_form.save(commit=False)
-            # profile.user = user
-            #
-            # # Now we save the UserProfile model instance.
-            # profile.save()
+                # # Now sort out the UserProfile instance.
+                # # Since we need to set the user attribute ourselves, we set commit=False.
+                # # This delays saving the model until we're ready to avoid integrity problems.
+                # profile = profile_form.save(commit=False)
+                # profile.user = user
+                #
+                # # Now we save the UserProfile model instance.
+                # profile.save()
 
-            # Update our variable to tell the template registration was successful.
-            registered = True
+                # Update our variable to tell the template registration was successful.
+                registered = True
 
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
+            # Invalid form or forms - mistakes or something else?
+            # Print problems to the terminal.
+            # They'll also be shown to the user.
+            else:
+                print(user_form.errors)
+
+        # Not a HTTP POST, so we render our form using two ModelForm instances.
+        # These forms will be blank, ready for user input.
         else:
-            print(user_form.errors)
+            user_form = UserForm()
 
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
+        # Render the template depending on the context.
+        return render(request,
+                'auth/registration.html',
+                {'user_form': user_form,
+                 'registered': registered} )
+    # if settings are production settings, then no registration is allowed.
     else:
-        user_form = UserForm()
+        messages.add_message(request,
+                             messages.ERROR,
+                             "No registration allowed outside of EASE")
+        return HttpResponseRedirect('/')
 
-    # Render the template depending on the context.
-    return render(request,
-            'auth/registration.html',
-            {'user_form': user_form,
-             'registered': registered} )
 
 #
 # @login_required
