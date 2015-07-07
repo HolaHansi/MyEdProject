2a92e244d9605318ee995a0352b255d5568baa9f
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-07-07 14:01:44 +0100

    Added function for efficiency testing
    Commented out

--- rooms/tasks.py
+++ rooms/tasks.py
@@ -5,6 +5,8 @@ from .table import update_room_table, update_building_table, merge_room_building
 @task
 def get_data():
     update_room_table()
+    print('Rooms saved')
     update_building_table()
+    print('Buildings saved')
     merge_room_building()
     return 'successfully updated database'
