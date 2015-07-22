019d90cddd19748aea877f25c4ed661d488836e9
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-07-22 10:22:34 +0100

    All of Judy's templates merged at /new
    Last commit at /new, now switching to new branch for continued
    devlopment.
    
    Note not all static files included yet.

--- new/urls.py
+++ new/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls import url
 from new import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^labs/$', views.pc_index),
+    url(r'^$', views.index, name='home'),
+    url(r'^labs/$', views.pc_index, name='labs'),
     url(r'^labs/filter/$', 'pc.views.filter_suggestions'),
-    url(r'^rooms/$', views.rooms_index),
+    url(r'^rooms/$', views.rooms_index, name='rooms'),
     url(r'^rooms/filter/$', 'rooms.views.filter_suggestions'),
 
-    url(r'^favourites/$', views.users_favourites),
+    url(r'^favourites/$', views.users_favourites, name='favourites'),
+    url(r'^history/$', views.users_history, name='history'),
     url(r'^like/', 'users.views.like'),
     url(r'^registration/$', 'users.views.register'),
     url(r'^logout/$', 'users.views.logout'),
