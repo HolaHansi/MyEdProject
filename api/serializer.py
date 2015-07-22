64c36df274242a17e83a08335cb2d3fd04e2dd72
HolaHansi <s1310721@sms.ed.ac.uk>
2015-07-22 10:17:35 +0100

    deleted api

--- api/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from pc.models import Computer_Labs
-
-
-class PC_Space_Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Computer_Labs
