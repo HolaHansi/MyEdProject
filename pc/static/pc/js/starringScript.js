dd1ebb0a4a8f8555d658c34e3ff2c45adc468dd0
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-07-24 10:25:08 +0100

    Fix arrow positioning
    And other wee tweaks

--- pc/static/pc/js/starringScript.js
+++ pc/static/pc/js/starringScript.js
@@ -5,21 +5,30 @@ $(document).ready(function () {
         $(this).toggleClass('starred')
     })
 
-    //to be moved:
-    $('.arrow').css({
-        'height': (window.innerHeight - 40) + 'px'
-    })
-
+    
     //For testing only:
     $('#currentMap').attr('src', 'https://maps.googleapis.com/maps/api/staticmap?size=' + (window.innerWidth - 60) + 'x300&key=AIzaSyBcrXTgUVxfXVLj3rh5gIUWyYRpveHMmEs&markers=size:medium%7Clabel:A%7C55.9460736605763,-3.20059955120087&markers=size:medium%7Clabel:B%7C55.9427113171065,-3.18914651870728')
     $('#currentMap').css({
         'height': '300px',
         'width': '2000px'
     })
+    
+    resizeElements();
 
     //make a pie chart
     makepie("computersFreeGraph", "15", "12");
+    
 })
+
+$(window).resize(resizeElements);
+
+function resizeElements(){
+    
+    $('.arrow').height(0);
+    //resize the arrows to take up the whole screen
+    $('.arrow').height(Math.max((window.innerHeight - 45),($('body').height()-45)));
+}
+
 
 function makepie(id, free, inuse) {
 
