294bd7668a4a4313d8b1a59a02c262091cfc4f7a
HolaHansi <s1310721@sms.ed.ac.uk>
2015-08-11 16:24:19 +0100

    Calendar now working

--- users/static/users/js/calendarScript.js
+++ users/static/users/js/calendarScript.js
@@ -65,6 +65,7 @@ $(document).ready(function(){
             right: 'agendaWeek,agendaDay'
         },
         defaultDate: dateNow,
+        // show week before day
         defaultView: 'agendaWeek',
         editable: false,
         businessHours: false,
