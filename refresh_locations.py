1b7f48e40ac76077d1e0e6b59c3d39404e854982
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-08-12 12:44:22 +0100

    Add Django environment to tasks

--- refresh_locations.py
+++ refresh_locations.py
@@ -1,5 +1,9 @@
 # This file is run by a Cron job every 24 hours
 
 from core.tasks import refresh_locations
+import os
+
+# set up the django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.development")
 
 refresh_locations()
