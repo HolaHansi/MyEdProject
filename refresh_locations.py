7f3253cc77d9c153c4efc86f2e0f87e864307f38
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-08-12 12:46:11 +0100

    Switch tasks to production only

--- refresh_locations.py
+++ refresh_locations.py
@@ -4,6 +4,6 @@ from core.tasks import refresh_locations
 import os
 
 # set up the django environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
 
 refresh_locations()
