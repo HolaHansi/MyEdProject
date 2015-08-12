5c683d71effa568418ad9cfdbb516c2ba3439b10
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-08-12 12:50:39 +0100

    Set up environment before import
    pls work this time

--- refresh_availability.py
+++ refresh_availability.py
@@ -1,9 +1,10 @@
 # This file is run by a Cron job every 24 hours
 
-from core.tasks import refresh_availability
 import os
 
 # set up the django environment
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
+
+from core.tasks import refresh_availability
 
 refresh_availability()
