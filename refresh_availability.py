18eefbeb7468bd8928608b24e4b0e98b05b63c81
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-08-12 12:56:36 +0100

    Add django.setup
    Maybe it'll work now..?

--- refresh_availability.py
+++ refresh_availability.py
@@ -1,8 +1,10 @@
 # This file is run by a Cron job every 24 hours
 
 import os
+import django
 
 # set up the django environment
+django.setup()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
 
 from core.tasks import refresh_availability
