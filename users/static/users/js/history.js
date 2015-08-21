4f3e3445a30aefc9ba3ef8317ebd14ea7861f137
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-08-21 11:00:11 +0100

    Toggle arrows on history panels
    Plus prettification

--- users/static/users/js/history.js
+++ users/static/users/js/history.js
@@ -1,12 +1,17 @@
-/**
- * Created by hanschristiangregersen on 07/08/15.
- */
-
 $(document).ready(function(){
     // style the favourites tab as the current tab
     $('#mainHamburgerMenuOptions .historyTab').addClass('currentTab');
 
+    // This will make sure that the arrow in each panel changes direction whenever the panel
+    // either collapses or unfolds.
+    $(".panel").on('show.bs.collapse', function(){
+        $(this).addClass('dropup');
+    });
 
+    $(".panel").on('hide.bs.collapse', function(){
+        $(this).removeClass('dropup');
+    });
+    
     $('#clearHistory').click(function(){
 
         $.post('/history/', {
