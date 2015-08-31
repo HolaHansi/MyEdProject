c6271393c96e332d031f0e03b2f718c1515c5371
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-08-31 17:32:26 +0100

    Tiny tweaks

--- core/static/core/js/booking.js
+++ core/static/core/js/booking.js
@@ -8,6 +8,8 @@ function bookRoom(id){
         'locationId': id,
         'clearAll': false
     }).done(function(data){
+        // TODO: Book the room
+        // until we have that functionality ourselves, just navigate to TED's booking page
         window.location.href = 'https://www.ted.is.ed.ac.uk/UOE1415STU_WRB/default.aspx';
     });
 }
