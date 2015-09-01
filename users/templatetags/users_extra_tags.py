5d8f88a36fd4e5a7e96e24dda37b61df3cf3b006
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-09-01 17:06:01 +0100

    Add screen reader info to glyphicons

--- users/templatetags/users_extra_tags.py
+++ users/templatetags/users_extra_tags.py
@@ -11,30 +11,16 @@ def facilities(room):
     # Returns an HTML list of all the facilities the room has
     to_return = ''
     if room.pc:
-        to_return += '<span class="custom-glyphicon glyphicon-computer" aria-hidden="true"></span> &nbsp; '
+        to_return += '<span class="custom-glyphicon glyphicon-computer" aria-hidden="true"></span><span class="sr-only">Computer</span> &nbsp; '
     if room.printer:
-        to_return += '<span class="custom-glyphicon glyphicon-printer" aria-hidden="true"></span> &nbsp; '
+        to_return += '<span class="custom-glyphicon glyphicon-printer" aria-hidden="true"></span><span class="sr-only">Printer</span> &nbsp; '
     if room.projector:
-        to_return += '<span class="custom-glyphicon glyphicon-projector" aria-hidden="true"></span>  &nbsp;'
+        to_return += '<span class="custom-glyphicon glyphicon-projector" aria-hidden="true"></span><span class="sr-only">Projector</span>  &nbsp;'
     if room.blackboard:
-        to_return += '<span class="custom-glyphicon glyphicon-blackboard-custom" aria-hidden="true"></span> &nbsp; '
+        to_return += '<span class="custom-glyphicon glyphicon-blackboard-custom" aria-hidden="true"></span><span class="sr-only">Blackboard</span> &nbsp; '
     if room.whiteboard:
-        to_return += '<span class="custom-glyphicon glyphicon-whiteboard" aria-hidden="true"></span> &nbsp; '
+        to_return += '<span class="custom-glyphicon glyphicon-whiteboard" aria-hidden="true"></span><span class="sr-only">Whiteboard</span> &nbsp; '
     return mark_safe(to_return)
-
-
-@register.filter
-def get_batch(room):
-    avail = room['availability']
-    if avail == 'availableNow':
-        html_return = '<span class="badge check"><i class="fa fa-check avail"></i></span>'
-    elif avail == 'notAvailable':
-        html_return = '<span class="badge times"><i class="fa fa-times"></i></span>'
-    elif avail == 'localAvailable':
-        html_return = '<span class="badge minus"><i class="fa fa-minus avail"></i></span>'
-    else:
-        html_return = "Error - unknown type of availability"
-    return mark_safe(html_return)
 
 
 @register.filter
@@ -68,28 +54,6 @@ def get_availFor(room):
 
 
 @register.filter
-def facilitiesRoom(room):
-    """
-    this is facilities for history which takes dictionary rather than an object.
-    :param room:
-    :return:
-    """
-    # Returns an HTML list of all the facilities the room has
-    to_return = ''
-    if room['pc']:
-        to_return += '<span class="custom-glyphicon glyphicon-computer" aria-hidden="true"></span> &nbsp; '
-    if room['printer']:
-        to_return += '<span class="custom-glyphicon glyphicon-printer" aria-hidden="true"></span> &nbsp; '
-    if room['projector']:
-        to_return += '<span class="custom-glyphicon glyphicon-projector" aria-hidden="true"></span>  &nbsp;'
-    if room['blackboard']:
-        to_return += '<span class="custom-glyphicon glyphicon-blackboard-custom" aria-hidden="true"></span> &nbsp; '
-    if room['whiteboard']:
-        to_return += '<span class="custom-glyphicon glyphicon-whiteboard" aria-hidden="true"></span> &nbsp; '
-    return mark_safe(to_return)
-
-
-@register.filter
 def booked_at_time(room):
     """
     returns the historical booking time for the room in proper format.
@@ -111,57 +75,6 @@ def booked_at_time(room):
     form_string = str(day) + '/' + str(month) + '/2015' + ' ' + str(hour) + ':' + str(minute)
 
     return form_string
-
-
-@register.filter
-def locally_allocated(room):
-    return room.locally_allocated
-
-
-@register.filter
-def openTimeRoom(room):
-    """
-    returns the current opening time.
-    :param room:
-    :return:
-    """
-    # if no opening hours known, return unkown
-    if not room['weekdayOpen']:
-        return 'n/a'
-
-    now = datetime.datetime.now()
-    weekday = now.weekday()
-    result = ''
-    if 0 <= weekday <= 4:
-        result = str(room['weekdayOpen'])[0:5]
-    elif weekday == 5:
-        result = str(room['saturdayOpen'])[0:5]
-    elif weekday == 6:
-        result = str(room['sundayOpen'])[0:5]
-    return result
-
-
-@register.filter
-def closingTimeRoom(room):
-    """
-    returns the current closing time.
-    :param room:
-    :return:
-    """
-    # if no opening hours known, return unkown
-    if not room['weekdayClosed']:
-        return 'n/a'
-
-    now = datetime.datetime.now()
-    weekday = now.weekday()
-    result = ''
-    if 0 <= weekday <= 4:
-        result = str(room['weekdayClosed'])[0:5]
-    elif weekday == 5:
-        result = str(room['saturdayClosed'])[0:5]
-    elif weekday == 6:
-        result = str(room['sundayClosed'])[0:5]
-    return result
 
 
 @register.filter
@@ -335,7 +248,7 @@ def get_id(room, type):
     returns the location id or history id
     :param room:
     :param type: 'favourite' or 'history'
-    :return: string: locatoin/history id
+    :return: string: location/history id
     """
     if type == 'history':
         return room.history_id
