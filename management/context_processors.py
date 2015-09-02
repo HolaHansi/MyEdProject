5712e8de9213da70a0f18a44768f060ef5d6d27e
pbsinclair42 <bobsmith0030+git2@gmail.com>
2015-09-02 11:11:25 +0100

    Send root URL using context processor

--- management/context_processors.py
+++ management/context_processors.py
@@ -4,3 +4,8 @@ from django.conf import settings
 # send the LABS_ONLY setting to all views
 def labs_only_processor(request):
     return {'labsOnly': settings.LABS_ONLY}
+
+
+# send the root url to all views
+def root_url_processor(request):
+    return {'rootURL': '//'+request.get_host()+'/'}
